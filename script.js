a87630309c5e3a323fdaa3571b93f85254c54303
Faxriddinxoja <faxriddin@gmail.com>
2024-03-06 11:52:52 +0300

    end

---
+++ script.js
@@ -0,0 +1,30 @@
+  
+  var name = "faxriddinxoja"
+  var age = 13
+  console.log(age + true);
+  console.log('5+3=' + (5 + 3));
+  console.log('5-3=' + (5 - 3));
+  console.log('5*3=' + 5 * 3);
+  console.log('8/4=' + 8 / 4);
+  console.log('15%4=' + 15 % 4);
+  var text = prompt('mani ismim faxriddinxoja')
+  console.log( text);
+  var text = prompt('sizi ismiz nima')
+  console.log('sizning ismiz nima' + text);
+  var text = prompt('siz misol yechishi bilasizmi')
+  console.log( 'siz misol yechishi bilasizmi' + text);
+  var misol = prompt('5 / 3=#')
+  console.log( '5 / 3= ' + misol);
+  var misol = prompt('4 + 3=#')
+  console.log( '4 + 3= ' + misol);
+  var misol = prompt('2 * 3=#')
+  console.log( '2 * 3= ' + misol);
+  var misol = prompt('6 * 3=#')
+  console.log( '6 * 3= ' + misol);
+  var misol = prompt('5 * 9=#')
+  console.log( '5 * 9= ' + misol);
+  var misol = prompt('12 * 3=#')
+  console.log( '12 * 3= ' + misol);
+  var text = prompt('raxmat yaxwi yechdingiz')
+  console.log( 'raxmat yaxwi yechdingiz'+ text);
+  
